204683c5faeb21bcd2939540fe0b9bf4f756f470
Danya Samoylov <130479785+Zshdun@users.noreply.github.com>
2024-06-15 16:00:10 +0700

    Add files via upload

---
+++ 1IMMOD12/12IMITAS/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace _12IMITAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelDays = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(56, 159);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(518, 404);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(635, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(221, 202);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество дней";
+            // 
+            // labelDays
+            // 
+            this.labelDays.AutoSize = true;
+            this.labelDays.Location = new System.Drawing.Point(429, 42);
+            this.labelDays.Name = "labelDays";
+            this.labelDays.Size = new System.Drawing.Size(35, 13);
+            this.labelDays.TabIndex = 5;
+            this.labelDays.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(321, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Эмперические вероятности";
+            // 
+            // emp
+            // 
+            this.emp.AutoSize = true;
+            this.emp.Location = new System.Drawing.Point(496, 92);
+            this.emp.Name = "emp";
+            this.emp.Size = new System.Drawing.Size(0, 13);
+            this.emp.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 604);
+            this.Controls.Add(this.emp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelDays);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelDays;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label emp;
+    }
+}
+
---
+++ 1IMMOD12/12IMITAS/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12IMITAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        int days, state, state0 = 0, stateOld;
+        int[] emperic = new int [3];
+        double[,] P =
+        {
+            {0.6, 0.3, 0.1},
+            {0.4, 0.2, 0.4},
+            {0.1, 0.4, 0.5}
+        };
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelDays.Visible = true;
+            emp.Visible = true;
+            labelDays.Text = days.ToString();
+            timer1.Stop();
+            for(int i = 0; i < emperic.Length; i++)
+            {
+                double text = (double)emperic[i] / days;
+                emp.Text += "Состояние " + i + ": " + Math.Round(text,2).ToString() + "    ";
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            state = State(state);
+            Picter(state);
+            days++;
+
+            if(state >=0 && state <=2)
+            {
+                emperic[state]++;
+            }
+
+            chart1.Series[0].Points.AddXY(days, state);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labelDays.Visible = false;
+            emp.Visible = false;
+            emp.Text = "";
+            days = 0;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(days, state0);
+            Picter(state0);
+
+            for(int i = 0; i < emperic.Length; i++)
+            {
+                emperic[i] = 0;
+            }
+
+            timer1.Start();
+        }
+
+        public int State(int oldSt)
+        {
+            double sum = 0;
+            double a = random.NextDouble();
+            for (int i = 0; i < 3; i++)
+            {
+                sum += P[oldSt, i];
+                if(a<sum)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Picter(int state)
+        {
+            switch (state)
+            {
+                case 0:
+                    pictureBox1.Image = Image.FromFile("E:\\Images\\1.jpg");
+                    break;
+                case 1:
+                    pictureBox1.Image = Image.FromFile("E:\\Images\\2.jpg");
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile("E:\\Images\\3.jpg");
+                    break;
+            }
+        }
+    }
+
+   
+}
